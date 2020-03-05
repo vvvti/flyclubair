dde0bf4db3aedbbd536f2c5fcc4078c0cfccccab
Patryk <patryk.kontewicz@poczta.fm>
2020-03-05 22:26:26 +0100

    add layer4 js and css

--- src/components/layout/Section_3/Layer4.js
+++ src/components/layout/Section_3/Layer4.js
@@ -6,6 +6,28 @@ class Layer4 extends Component {
   render() {
     return (
       <div className="layer-4">
+        <div class="layer4-content">
+          <div className="layer4-header">
+            <h1>Why fly with us?</h1>
+          </div>
+          <div className="layer4-list">
+            <ul>
+              <li>Book in seconds</li>
+              <li>Flexible changes</li>
+              <li>No fees</li>
+              <li>No lines or waiting</li>
+            </ul>
+          </div>
+          <div className="layer4-list">
+            <ul>
+              <li>Personalised concierge</li>
+              <li>Private aviation terminals</li>
+              <li>Executive aircraft</li>
+              <li>Beverages and amenities</li>
+            </ul>
+          </div>
+        </div>
+        <div className="layer4-list"></div>
         <img src={layer_4} alt="layer_4"></img>
       </div>
     );
